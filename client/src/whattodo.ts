f9373ed565614dedd00782156a2cc7a7a85d564e
undstory <undrul.ozog@gmail.com>
2026-04-23 13:29:35 +0200

    updated todo list

--- client/src/whattodo.ts
+++ client/src/whattodo.ts
@@ -1,5 +1,4 @@
 // zbyt duza ilosc linkow rozpycha strone, trzeba zrobic paginacje
 // trezba dodac sortowanie i filtrowanie do listy wszytskich linkow, ale tak by bylo reuzywalne
 // trzeba dodac mozliwosc edycji linkow
-// ogarnac dodawanie tagow
 // refactor - hooki
